a619aa35c21131b2ecf8e3c7a5f8cfa877e3835f
Enes Kapucu <enes.kapucu@outlook.com.tr>
2017-08-10 17:14:59 +0300

    added multi selector

--- dist/jquery.dblClickField.js
+++ dist/jquery.dblClickField.js
@@ -5,47 +5,49 @@
 (function ($) {
     "use strict";
     $.fn.dblClickField = function (param) {
-        var thisField = $(this);
-        thisField.after('<textarea class="dcf-editArea"></textarea>');
-        var textFieldNext = thisField.next('textarea.dcf-editArea');
-        textFieldNext.css({
-            'font-family': thisField.css('font-family'),
-            'font-size': thisField.css('font-size'),
-            'text-align': thisField.css('text-align'),
-            'font-weight': 700
-        });
-        autosize(textFieldNext);
-        thisField.dblclick(function () {
-            textFieldNext.show();
-            textFieldNext.focus();
-            textFieldNext.val(thisField.text());
-            thisField.hide();
-        });
-        textFieldNext.keypress(function (event) {
-            if (event.which == 13) {
-                event.preventDefault();
-                if (!$.isEmptyObject(param) && !$.isEmptyObject(param.send)) {
-                    var name = (!$.isEmptyObject(thisField.attr('sname')) ? thisField.attr('sname') : 'dcf1');
-                    $.ajax({
-                        url: param.send.url,
-                        method: param.send.method,
-                        data: textFieldNext.attr('name', name).serializeArray()
-                    }).done(function (response) {
-                        var r = $.parseJSON(response);
-                        if (r.success) {
-                            thisField.text(textFieldNext.val());
-                            thisField.show();
-                            textFieldNext.hide();
-                        } else {
-                            alert(r.errorMessage);
-                        }
-                    });
-                } else {
-                    thisField.text(textFieldNext.val());
-                    thisField.show();
-                    textFieldNext.hide();
+        this.each(function () {
+            var thisField = $(this);
+            thisField.after('<textarea class="dcf-editArea"></textarea>');
+            var textFieldNext = thisField.next('textarea.dcf-editArea');
+            textFieldNext.css({
+                'font-family': thisField.css('font-family'),
+                'font-size': thisField.css('font-size'),
+                'text-align': thisField.css('text-align'),
+                'font-weight': 700
+            });
+            autosize(textFieldNext);
+            thisField.dblclick(function () {
+                textFieldNext.show();
+                textFieldNext.focus();
+                textFieldNext.val(thisField.text());
+                thisField.hide();
+            });
+            textFieldNext.keypress(function (event) {
+                if (event.which == 13) {
+                    event.preventDefault();
+                    if (!$.isEmptyObject(param) && !$.isEmptyObject(param.send)) {
+                        var name = (!$.isEmptyObject(thisField.attr('sname')) ? thisField.attr('sname') : 'dcf1');
+                        $.ajax({
+                            url: param.send.url,
+                            method: param.send.method,
+                            data: textFieldNext.attr('name', name).serializeArray()
+                        }).done(function (response) {
+                            var r = $.parseJSON(response);
+                            if (r.success) {
+                                thisField.text(textFieldNext.val());
+                                thisField.show();
+                                textFieldNext.hide();
+                            } else {
+                                alert(r.errorMessage);
+                            }
+                        });
+                    } else {
+                        thisField.text(textFieldNext.val());
+                        thisField.show();
+                        textFieldNext.hide();
+                    }
                 }
-            }
+            });
         });
     }
 })(jQuery);
